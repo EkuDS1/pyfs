e518e81abf6d11e754a1d6715c84ba61f45da3a0
Vin-Xi <45799230+Vin-Xi@users.noreply.github.com>
2020-11-27 19:23:50 +0500

    Hamdan Rashid's Commit

---
+++ FileSystem.py
@@ -0,0 +1,87 @@
+import os
+import pickle
+
+
+class Node:
+  def __init__(self):
+    self.startingAddress=None
+    self.offset=None
+   
+  def setOffset(self,offset):
+    self.offset=offset
+
+  def setStartingAddress(self,sA):
+    self.startingAddress=sA
+
+  def getOffset(self):
+    return self.offset
+
+  def getStartingAddress(self):
+    return self.startingAddress
+  
+  def getLimit(self):
+    return self.startingAddress+self.offset
+  
+
+
+class FileSystem:
+  global dicn
+  dicn=dict()
+  def __init__(self): 
+    dicn['initial_size']=128
+
+  
+  def createFile(self):
+    node=Node()
+    virtualHardDick=open("testfile","r+b")
+    #File Data
+    name=input("Please enter a File Name:")
+    text=input("Please enter the data you want to store:")
+    #Adding Node Info
+    node.setStartingAddress(dicn['initial_size'])
+    node.setOffset(len(text))
+    #Adding Data to VirtualHardDisk
+    dicn[name]=node
+    virtualHardDick.seek(dicn['initial_size'])
+    text=text.encode('utf-8')
+    virtualHardDick.write(text)
+    dicn['initial_size']+=len(text)
+    
+
+  def readFile(self):
+    name=input("Enter the file you want to open")
+    virtualHardDick=open("testfile","r+b")
+    node=Node()
+    node=dicn[name]
+    startingAddress=node.getStartingAddress()
+    virtualHardDick.seek(startingAddress)
+    offset=node.getOffset()
+    text=virtualHardDick.read(offset)
+    text=text.decode('utf-8')
+    print(text)
+
+
+  def dumpFile(self):
+    fie=open("testfile","r+b")
+    dump=pickle.dumps(dicn,protocol=2)
+    print(os.path.getsize("testfile"))
+    fie.write(dump)
+    print(len(dicn))
+    print(len(dump))
+    fie.close()
+
+  def readDump(self):
+    with open("testfile","rb") as inputfie:
+      print(inputfie.read())
+      inputfie.seek(0)
+      dicnx=pickle.load(inputfie)
+      print(dicnx)
+      
+
+if __name__=="__main__":
+  
+  Fmds=FileSystem()
+  Fmds.createFile()
+  Fmds.readFile()
+  
+  print(os.path.getsize("testfile"))
